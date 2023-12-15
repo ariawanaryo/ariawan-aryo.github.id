d90edb85e9a0691faa371ffbd4edbc4336e67f35
ariawanaryo <153246436+ariawanaryo@users.noreply.github.com>
2023-12-15 08:57:13 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,62 @@
+<?php include 'header.php' ;?>
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container text-center">
+            <img class="rounded-circle" width="300" height="300" src="https://www.pili.or.id/images/kabar_berita/elang4.jpg" alt="">
+          <h1 class="display-4">Elang Jawa</h1>
+          <p class="lead">Elang jawa adalah burung langka hampir punah dari pulau jawa.</p>
+        </div>
+      </div>
+      <div class="container text-center" id="about">
+        <div class="row">
+        <div class="col">
+        <h1>About</h1>
+
+      </div>
+
+      <div class="row">
+        <div class="col">
+            <p>Elang Jawa atau yang bahasa ilmiahnya Nisaetus Bartelsi merupakan salah satu spesies elang yang endemik di Indonesia, yaitu keberadaannya di Pulau Jawa.</p>
+
+        </div>
+
+        
+        <div row="col">
+        <p>Elang adalah hewan berdarah panas, mempunyai sayap dan tubuh yang diselubungi bulu pelepah. Sebagai burung, elang berkembang biak dengan cara bertelur yang mempunyai cangkang keras di dalam sarang yang dibuatnya. Ia menjaga anaknya sampai mampu terbang. Elang merupakan hewan pemangsa.</p>
+            </div>
+      </div>
+     
+        </div>
+
+        <div class="card-group">
+            <div class="card">
+              <img class="card-img-top" src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Haliaeetus_leucogaster_-Gippsland%2C_Victoria%2C_Australia-8.jpg/640px-Haliaeetus_leucogaster_-Gippsland%2C_Victoria%2C_Australia-8.jpg" alt="Card image cap">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+              </div>
+            </div>
+            <div class="card">
+              <img class="card-img-top" src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Haliaeetus_leucogaster_-Gippsland%2C_Victoria%2C_Australia-8.jpg/640px-Haliaeetus_leucogaster_-Gippsland%2C_Victoria%2C_Australia-8.jpg" alt="Card image cap">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+              </div>
+            </div>
+            <div class="card">
+              <img class="card-img-top" src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Haliaeetus_leucogaster_-Gippsland%2C_Victoria%2C_Australia-8.jpg/640px-Haliaeetus_leucogaster_-Gippsland%2C_Victoria%2C_Australia-8.jpg" alt="Card image cap">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+              </div>
+            </div>
+          </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <?php include 'footer.php' ;?>
